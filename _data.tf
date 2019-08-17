eb0e2a073aeb3aa40392a6e4cd62f1aa8025ed4e
Allan Denot <adenot@gmail.com>
2019-08-17 15:55:58 +1000

    Adjusting cloudwatch alarm

--- _data.tf
+++ _data.tf
@@ -11,6 +11,7 @@ data "aws_ami" "amzn" {
 }
 
 data "aws_caller_identity" "current" {}
+data "aws_iam_account_alias" "current" {}
 
 #-------
 # KMS
--- cloutwatch-alarms.tf
+++ cloutwatch-alarms.tf
@@ -1,12 +1,12 @@
 resource "aws_cloudwatch_metric_alarm" "high_memory" {
   count = "${var.alarm_sns_topic == "" ? 0 : 1}"
 
-  alarm_name                = "ecs-${var.name}-high-memory"
+  alarm_name                = "${data.aws_iam_account_alias.current.account_alias}-ecs-${var.name}-high-memory"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "3"
   metric_name               = "MemoryUtilization"
   namespace                 = "AWS/ECS"
-  period                    = "600"
+  period                    = "300"
   statistic                 = "Maximum"
   threshold                 = "80"
   alarm_description         = "Cluster node memory above threshold"
@@ -20,12 +20,12 @@ resource "aws_cloudwatch_metric_alarm" "high_memory" {
 resource "aws_cloudwatch_metric_alarm" "high_cpu" {
   count = "${var.alarm_sns_topic == "" ? 0 : 1}"
 
-  alarm_name                = "ecs-${var.name}-high-cpu"
+  alarm_name                = "${data.aws_iam_account_alias.current.account_alias}-ecs-${var.name}-high-cpu"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "3"
   metric_name               = "CPUUtilization"
   namespace                 = "AWS/ECS"
-  period                    = "600"
+  period                    = "300"
   statistic                 = "Maximum"
   threshold                 = "80"
   alarm_description         = "Cluster node CPU above threshold"
