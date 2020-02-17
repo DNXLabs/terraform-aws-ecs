f15fa1eb76f00587bab8e0319eca901ee4641e36
Allan Denot <adenot@gmail.com>
2020-02-17 17:43:51 +1100

    Adding disk volume size as a parameter

--- _variables.tf
+++ _variables.tf
@@ -20,11 +20,11 @@ variable "on_demand_percentage" {
   description = "Percentage of on-demand intances vs spot"
   default     = 100
 }
+
 variable "on_demand_base_capacity" {
   description = "You can designate a base portion of your total capacity as On-Demand. As the group scales, per your settings, the base portion is provisioned first, while additional On-Demand capacity is percentage-based."
   default     = 0
 }
-
 
 variable "vpc_id" {
   description = "VPC ID to deploy the ECS cluster"
@@ -64,6 +64,7 @@ variable "alb" {
   default     = true
   description = "Whether to deploy an ALB or not with the cluster"
 }
+
 variable "alb_only" {
   default     = false
   description = "Whether to deploy only an alb and no cloudFront or not with the cluster"
@@ -85,20 +86,21 @@ variable "asg_memory_target" {
 }
 
 variable "alarm_sns_topics" {
-  default = []
+  default     = []
   description = "Alarm topics to create and alert on ECS instance metrics"
 }
 
 variable "expire_backup_efs" {
-  default = 0
+  default     = 0
   description = "Number of days the backup will be expired"
 }
 
 variable "target_group_arns" {
-  default = []
-  type = "list"
+  default     = []
+  type        = "list"
   description = "List of target groups for ASG to register"
 }
+
 variable "autoscaling_health_check_grace_period" {
   default     = 300
   description = "The length of time that Auto Scaling waits before checking an instance's health status. The grace period begins when an instance comes into service"
@@ -107,4 +109,9 @@ variable "autoscaling_health_check_grace_period" {
 variable "autoscaling_default_cooldown" {
   default     = 300
   description = "The amount of time, in seconds, after a scaling activity completes before another scaling activity can start"
+}
+
+variable "instance_volume_size" {
+  description = "Volume size for instances (in GB)"
+  default     = 20
 }
--- ec2-launch-template.tf
+++ ec2-launch-template.tf
@@ -17,6 +17,14 @@ resource "aws_launch_template" "ecs" {
     name = "${aws_iam_instance_profile.ecs.name}"
   }
 
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = "${var.instance_volume_size}"
+    }
+  }
+
   vpc_security_group_ids = concat(list(aws_security_group.ecs_nodes.id), var.security_group_ids)
 
   user_data = "${base64encode(data.template_file.userdata.rendered)}"
