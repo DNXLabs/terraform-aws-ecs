08b41c13406dcf19c610cc1e6ee6d8d58986b8ce
Allan Denot <adenot@gmail.com>
2020-02-26 08:30:40 +1100

    Commenting optional elements for now

--- cf-exports.tf
+++ cf-exports.tf
@@ -4,11 +4,11 @@ resource "aws_cloudformation_stack" "tf_exports" {
   template_body = templatefile("${path.module}/cf-exports.yml", {
     "name" = var.name,
     "vars" = {
-      "AlbId"                 = element(concat(aws_lb.ecs.*.id, [""]), 0),
-      "AlbArn"                = element(concat(aws_lb.ecs.*.arn, [""]), 0),
-      "AlbDnsName"            = element(concat(aws_lb.ecs.*.dns_name, [""]), 0),
-      "AlbZoneId"             = element(concat(aws_lb.ecs.*.zone_id, [""]), 0),
-      "AlbSecgrpId"           = element(concat(aws_security_group.alb.*.id, [""]), 0),
+      # "AlbId"                 = element(concat(aws_lb.ecs.*.id, [""]), 0),
+      # "AlbArn"                = element(concat(aws_lb.ecs.*.arn, [""]), 0),
+      # "AlbDnsName"            = element(concat(aws_lb.ecs.*.dns_name, [""]), 0),
+      # "AlbZoneId"             = element(concat(aws_lb.ecs.*.zone_id, [""]), 0),
+      # "AlbSecgrpId"           = element(concat(aws_security_group.alb.*.id, [""]), 0),
       "EcsIamRoleArn"         = aws_iam_role.ecs.arn,
       "EcsIamRoleName"        = aws_iam_role.ecs.name,
       "EcsServiceIamRoleArn"  = aws_iam_role.ecs_service.arn,
@@ -18,9 +18,9 @@ resource "aws_cloudformation_stack" "tf_exports" {
       "EcsId"                 = aws_ecs_cluster.ecs.id,
       "EcsName"               = aws_ecs_cluster.ecs.name,
       "EcsArn"                = aws_ecs_cluster.ecs.arn,
-      "AlbListenerHttpsArn"   = element(concat(aws_lb_listener.ecs_https.*.arn, [""]), 0),
-      "EcsNodesSecGrpId"      = aws_security_group.ecs_nodes.id,
-      "VpcId"                 = var.vpc_id
+      # "AlbListenerHttpsArn"   = element(concat(aws_lb_listener.ecs_https.*.arn, [""]), 0),
+      "EcsNodesSecGrpId" = aws_security_group.ecs_nodes.id,
+      "VpcId"            = var.vpc_id
     }
   })
 }
