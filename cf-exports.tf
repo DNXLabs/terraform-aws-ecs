b1a71e63b650ed2f9a343aa1993c4c3cbd12b801
Arthur Diniz <arthurbdiniz@gmail.com>
2020-02-26 12:52:25 +1100

    Check if aws_lb variables are null before export
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- cf-exports.tf
+++ cf-exports.tf
@@ -4,23 +4,23 @@ resource "aws_cloudformation_stack" "tf_exports" {
   template_body = templatefile("${path.module}/cf-exports.yml", {
     "name" = var.name,
     "vars" = {
-      # "AlbId"                 = element(concat(aws_lb.ecs.*.id, [""]), 0),
-      # "AlbArn"                = element(concat(aws_lb.ecs.*.arn, [""]), 0),
-      # "AlbDnsName"            = element(concat(aws_lb.ecs.*.dns_name, [""]), 0),
-      # "AlbZoneId"             = element(concat(aws_lb.ecs.*.zone_id, [""]), 0),
-      # "AlbSecgrpId"           = element(concat(aws_security_group.alb.*.id, [""]), 0),
+      "AlbId"                 = length(aws_lb.ecs[*].id) > 0 ? element(aws_lb.ecs[*].id, 0) : "undefined",
+      "AlbArn"                = length(aws_lb.ecs[*].arn) > 0 ? element(aws_lb.ecs[*].arn, 0) : "undefined",
+      "AlbDnsName"            = length(aws_lb.ecs[*].dns_name) > 0 ? element(aws_lb.ecs[*].dns_name, 0) : "undefined",
+      "AlbZoneId"             = length(aws_lb.ecs[*].zone_id) > 0 ? element(aws_lb.ecs[*].zone_id, 0) : "undefined",
+      "AlbSecgrpId"           = length(aws_security_group.alb[*].id) > 0 ? element(aws_security_group.alb[*].id, 0) : "undefined",
       "EcsIamRoleArn"         = aws_iam_role.ecs.arn,
       "EcsIamRoleName"        = aws_iam_role.ecs.name,
       "EcsServiceIamRoleArn"  = aws_iam_role.ecs_service.arn,
       "EcsServiceIamRoleName" = aws_iam_role.ecs_service.name,
       "EcsTaskIamRoleArn"     = aws_iam_role.ecs_task.arn,
       "EcsTaskIamRoleName"    = aws_iam_role.ecs_task.name,
-      "EcsId"                 = aws_ecs_cluster.ecs.id,
-      "EcsName"               = aws_ecs_cluster.ecs.name,
-      "EcsArn"                = aws_ecs_cluster.ecs.arn,
-      # "AlbListenerHttpsArn"   = element(concat(aws_lb_listener.ecs_https.*.arn, [""]), 0),
-      "EcsNodesSecGrpId" = aws_security_group.ecs_nodes.id,
-      "VpcId"            = var.vpc_id
+      "EcsId"                 = aws_ecs_cluster.ecs.*.id[0],
+      "EcsName"               = aws_ecs_cluster.ecs.*.name[0],
+      "EcsArn"                = aws_ecs_cluster.ecs.*.arn[0],
+      "AlbListenerHttpsArn"   = length(aws_lb_listener.ecs_https[*].arn) > 0 ? element(aws_lb_listener.ecs_https[*].arn, 0) : "undefined",
+      "EcsNodesSecGrpId"      = aws_security_group.ecs_nodes.id,
+      "VpcId"                 = var.vpc_id
     }
   })
 }
