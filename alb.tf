886496554c6acd9bd4589fdff99ce3318b41422c
Allan Denot <adenot@gmail.com>
2020-03-28 17:05:16 +1100

    Incorrect substring trimming first char

--- alb.tf
+++ alb.tf
@@ -70,7 +70,7 @@ resource "aws_lb_listener" "ecs_test_https" {
   certificate_arn   = var.certificate_arn
 
   default_action {
-    type             = "forward"
+    type = "forward"
     #target_group_arn = aws_lb_target_group.ecs_replacement_https[0].arn
     target_group_arn = aws_lb_target_group.ecs_default_https[0].arn
   }
@@ -104,7 +104,7 @@ resource "random_string" "alb_prefix" {
 resource "aws_lb_target_group" "ecs_default_http" {
   count = var.alb ? 1 : 0
 
-  name     = substr("ecs-${var.name}-default-http-${random_string.alb_prefix.result}",1,32)
+  name     = substr("ecs-${var.name}-default-http-${random_string.alb_prefix.result}", 0, 32)
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
@@ -117,7 +117,7 @@ resource "aws_lb_target_group" "ecs_default_http" {
 resource "aws_lb_target_group" "ecs_default_https" {
   count = var.alb ? 1 : 0
 
-  name     = substr("ecs-${var.name}-default-https-${random_string.alb_prefix.result}",1,32)
+  name     = substr("ecs-${var.name}-default-https-${random_string.alb_prefix.result}", 0, 32)
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
