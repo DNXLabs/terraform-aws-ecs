722dbd2684ce3de131aeaeb336f79ea7429e6095
Allan Denot <adenot@gmail.com>
2020-05-01 15:09:34 +1000

    Missing ALB security group inbound rule on ECS node

--- sg-ecs-nodes.tf
+++ sg-ecs-nodes.tf
@@ -20,6 +20,18 @@ resource "aws_security_group_rule" "all_from_alb_to_ecs_nodes" {
   source_security_group_id = aws_security_group.alb[0].id
 }
 
+resource "aws_security_group_rule" "all_from_alb_internal_to_ecs_nodes" {
+  count = var.alb_internal ? 1 : 0
+
+  description              = "from internal ALB"
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.ecs_nodes.id
+  source_security_group_id = aws_security_group.alb_internal[0].id
+}
+
 resource "aws_security_group_rule" "all_from_ecs_nodes_to_ecs_nodes" {
   description              = "Traffic between ECS nodes"
   type                     = "ingress"
