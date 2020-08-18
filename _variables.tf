153dab79bcd8e95988cb32ccbd5e1337c7b6410d
Vishal Bhogate <vishal.bhogate@brighte.com.au>
2020-08-18 12:15:16 +1000

    Adding certificate_internal_arn variable for internal ALB

--- _variables.tf
+++ _variables.tf
@@ -75,6 +75,11 @@ variable "alb_internal" {
   description = "Deploys a second internal ALB for private APIs"
 }
 
+variable "certificate_internal_arn" {
+  default     = null
+  description = "certificate arn for internal ALB (If alb_internal is true provide the certificate arn)"
+}
+
 variable "alb_ssl_policy" {
   default     = "ELBSecurityPolicy-2016-08"
   type        = string
