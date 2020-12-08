88df4a12e87a72477f4f03cce26a393192e0feb6
Arthur Diniz <arthurbdiniz@gmail.com>
2020-12-08 18:10:54 -0300

    Capacity provider feature (#22)
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- ecs.tf
+++ ecs.tf
@@ -1,5 +1,9 @@
 resource "aws_ecs_cluster" "ecs" {
-  name = var.name
+
+  depends_on = [aws_autoscaling_group.ecs, aws_ecs_capacity_provider.ecs_capacity_provider]
+  name       = var.name
+
+  capacity_providers = ["${var.name}-capacity-provider"]
 
   lifecycle {
     ignore_changes = [
