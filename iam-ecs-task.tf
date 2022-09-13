06da349c543927e0d0ef8eecde3b9152c88eebbc
Luiz Rocha <luizfel@gmail.com>
2022-09-13 13:44:37 +1000

    Add extra task policies (#41)
    * Add extra task policies
    
    Co-authored-by: lzrocha <lzrocha@users.noreply.github.com>

--- iam-ecs-task.tf
+++ iam-ecs-task.tf
@@ -88,3 +88,9 @@ resource "aws_iam_role_policy" "s3_policy" {
 }
 EOF
 }
+
+resource "aws_iam_role_policy_attachment" "extra_task_policies_arn" {
+  for_each   = toset(try(var.extra_task_policies_arn, []))
+  role       = aws_iam_role.ecs_task.name
+  policy_arn = each.key
+}
