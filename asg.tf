713091e5a057174e346e090303e4e39b0c6543c3
brightshri <shrikanth.mannari@brighte.com.au>
2025-11-20 09:55:31 +1100

    Add spot_allocation_strategy variable with default price-capacity-optimized
    - Add spot_allocation_strategy variable to _variables.tf with default value 'price-capacity-optimized'
    - Update asg.tf to use the new variable in instances_distribution block
    - Remove spot_instance_pools parameter as it's not compatible with price-capacity-optimized
    - Addresses AWS deprecation of 'lowest-price' spot allocation strategy

--- asg.tf
+++ asg.tf
@@ -18,7 +18,7 @@ resource "aws_autoscaling_group" "ecs" {
     }
 
     instances_distribution {
-      spot_instance_pools                      = 3
+      spot_allocation_strategy                 = var.spot_allocation_strategy
       on_demand_base_capacity                  = var.on_demand_base_capacity
       on_demand_percentage_above_base_capacity = var.on_demand_percentage
     }
